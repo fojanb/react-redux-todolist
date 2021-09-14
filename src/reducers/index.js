cb9730662c155776c923267ffba70cbd3b584316
Fojan Babaali <fozhanmail@gmail.com>
2021-09-13 20:21:42 -0700

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import addDeleteReducer from "./addDeleteReducer";
+import inputReducer from "./inputReducer";
 import { combineReducers } from "redux";
 const allReducers = combineReducers({
   state1: addDeleteReducer,
+  state2 : inputReducer,
 });
 export default allReducers;
