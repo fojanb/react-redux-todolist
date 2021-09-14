ced85674076100da7831479bc6a8bfc826032365
Fojan Babaali <fozhanmail@gmail.com>
2021-09-13 21:23:22 -0700

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,28 +2,24 @@ import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 function App() {
   const state1 = useSelector((state) => state.state1);
-  const state2 = useSelector((state) => state.state2);
   const dispatch = useDispatch();
   const addNewTask = () => {
     let newTask = document.createElement("div");
-    newTask.innerText = state2;
+    newTask.innerText = state1;
     document.querySelector(".tasks").appendChild(newTask);
   };
   return (
     <div className="App">
-      <div>
-        <input
+      <div className="wrapper">
+        <input className="bar"
           onChange={(e) =>
             dispatch({ type: "NEW_ITEM", EVENT: e.target.value })
           }
         ></input>
-        <button onClick={addNewTask}>Add new task</button>
-        <div className="tasks"></div>
+        <button className="addBtn"onClick={addNewTask}>Add</button>
       </div>
+      <div className="tasks"></div>
 
-      <button onClick={() => dispatch({ type: "INC" })}>+</button>
-      <button onClick={() => dispatch({ type: "DEC" })}>-</button>
-      {state1}
     </div>
   );
 }
