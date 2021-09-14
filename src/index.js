20d0ca274af73ee12027a7b25fdc86b5c4de3120
Fojan Babaali <fozhanmail@gmail.com>
2021-09-13 21:45:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,12 @@ import App from "./containers/App";
 import store from "./store/store";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-
 ReactDOM.render(
   <React.StrictMode>
-    <h2>My To-Do-List</h2>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
