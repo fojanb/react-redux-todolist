3c2e8fdb3137e286ef9b0351f4b3964ed0062148
Fojan Babaali <fozhanmail@gmail.com>
2021-09-13 18:54:43 -0700

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,18 +4,18 @@ import { useSelector, useDispatch } from "react-redux";
 function App() {
   const state1 = useSelector((state) => state.state1);
   const dispatch = useDispatch();
-  const inputHandler = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-  };
+  // const inputHandler = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target.value);
+  // };
   return (
     <div className="App">
-      <form>
+      
         Add your items
-        <input onChange={inputHandler}></input>
-        <button onClick={() => dispatch({ type: "ADD_ITEM" })}>ADD ITEM</button>
         {state1}
-      </form>
+
+        <button onClick={() => dispatch({ type: "ADD_ITEM" })}>ADD ITEM</button>
+      
     </div>
   );
 }
