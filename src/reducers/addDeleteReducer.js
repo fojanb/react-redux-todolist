5c9e53843dc33b99029849bd6ac0083df15dfd6d
Fojan Babaali <fozhanmail@gmail.com>
2021-09-13 19:14:30 -0700

    Create addDeleteReducer.js

---
+++ src/reducers/addDeleteReducer.js
@@ -0,0 +1,14 @@
+const addDeleteReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      state = state + 1;
+      break;
+    case "DELETE_ITEM":
+      state = state - 1;
+      break;
+    default:
+      return state;
+  }
+  return state;
+};
+export default addDeleteReducer;
